a324f9bf6e7b9c9f294f4e7597bf4f55aadda23a
Manuel <[manuel.galeana2590@gmail.com]>
2020-06-29 00:38:07 -0500

    mi primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+
+
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+
+document.querySelector('.nav-menu ul').classList.toggle('show');
+
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.container-div', {delay: 500});
+ScrollReveal().reveal('.banner img', {delay: 500});
+ScrollReveal().reveal('.ptat img', {delay: 500});
